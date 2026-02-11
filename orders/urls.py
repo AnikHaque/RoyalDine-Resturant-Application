969677e341eeaba8e9058f36fa89e5047c697360
AnikHaque <ekramul15-811@diu.edu.bd>
2026-02-12 01:33:06 +0600

    payment part done

--- orders/urls.py
+++ orders/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from .views import checkout_view
 
 urlpatterns = [
+    
     path('checkout/', checkout_view, name='checkout'),
     path('pay/<int:order_id>/', views.payment_page, name='payment_page'),
     path('success/', views.payment_success, name='payment_success'),
