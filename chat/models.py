e9d7ebe71145e8ea57a78a62167f5d39a6ab31f6
AnikHaque <ekramul15-811@diu.edu.bd>
2026-02-17 23:04:21 +0600

    conversation & message both model done

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Conversation(models.Model):
+    participants = models.ManyToManyField(User, related_name='conversations')
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Chat between: {', '.join([u.username for u in self.participants.all()])}"
+
+class Message(models.Model):
+    conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name='messages')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    is_read = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['timestamp']
+
+    def __str__(self):
+        return f"{self.sender.username}: {self.text[:20]}"
